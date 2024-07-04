5b67d7cc7a84a19096e40e875d827973187e9359
Josselin Dionisi <dionisi.josselin@gmail.com>
2024-07-04 13:52:20 +0200

    Add codemirror for edit with format

--- src/components/EditSnippet.js
+++ src/components/EditSnippet.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/theme/neat.css';
+import 'codemirror/mode/xml/xml.js';
+import 'codemirror/mode/htmlmixed/htmlmixed.js';
+import 'codemirror/mode/php/php.js';
+import 'codemirror/mode/javascript/javascript.js';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
 
 function EditSnippetForm({ snippet, onSave, onCancel }) {
   const [code, setCode] = useState(snippet.code);
@@ -10,10 +18,17 @@ function EditSnippetForm({ snippet, onSave, onCancel }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Langage: {snippet.language}
-      </label>
-      <textarea value={code} onChange={(e) => setCode(e.target.value)} />
+      <CodeMirror
+        value={code}
+        options={{
+          mode: snippet.language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onChange={(editor, data, value) => {
+          setCode(value);
+        }}
+      />
       <button type="button" onClick={onCancel}>Annuler</button>
       <button type="submit">Sauvegarder</button>
     </form>
